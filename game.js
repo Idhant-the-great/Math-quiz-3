03840f5b8eda7b07b9a118a7369d55fe2ccff778
Idhant-the-great <82503254+Idhant-the-great@users.noreply.github.com>
2021-10-06 19:21:41 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -1,12 +1,19 @@
+player_1=localStorage.getItem("name_1")
+player_2=localStorage.getItem("name_2")
+document.getElementById("player1_name").innerHTML=player_1+":"+"0";
+document.getElementById("player2_name").innerHTML=player_2+":"+"0";
+
+
 function send()
 {
-    number1=document.getElementById("number1").value;
-    number1=document.getElementById("number2").value;
+    number1=document.getElementById("first_input").value;
+    number2=document.getElementById("second_input").value;
     actual_answer=parseInt(number1)*parseInt(number2);
     question_number="<h4>" + number1 + "X" + number2+"</h4>";
     input_box="<br> Answer:<input type='text' id='input_check_box'>";
     check_button="<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
     row= question_number+input_box+check_button;
+    document.getElementById("output").innerHTML=row;
     document.getElementById("number1").value="";
     document.getElementById("number2").value="";    
 }
